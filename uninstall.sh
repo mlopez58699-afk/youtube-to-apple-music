e0a54182f2773306a7d3634f5ba16c9d7b405490
Moises Lopez <moi21@MacBook-Pro-3.local>
2026-07-18 00:04:16 -0700

    Add version file and uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Removing YouTube to Apple Music..."
+
+rm -rf "$HOME/.youtube-to-apple-music"
+rm -f "$HOME/.local/bin/ytmusic"
+
+echo
+echo "Uninstalled successfully."
+
+echo
+echo "You may also remove this repository folder if you no longer need it."
